a0114f2fd626a5c1600c0f91cc4a2272e9d5090a
dem-14 <60597957+dem-14@users.noreply.github.com>
2020-07-31 09:41:09 +0200

    tests notauthorized realizados

---
+++ httpnotauthorized.test.js
@@ -0,0 +1,11 @@
+//https://stackoverflow.com/questions/49886244/how-to-test-class-constructor-in-jest
+const NotAutorized = require('./httpnotauthorized')
+test("testing NotAutorized new NotAutorized(message= Not autorized)", () => {
+        // arrange 
+        const message = "Not autorized"
+        // act
+        const classInstance = new NotAutorized(message);
+        // assert
+        expect(classInstance.message).toBe(message);
+        expect(classInstance.code).toBe(401);
+})
